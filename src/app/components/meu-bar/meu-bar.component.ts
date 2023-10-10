9a76643c0f53fcdaaeb421fc576a5fd8bfaf8962
ivens magno <ivensmagno@gmail.com>
2023-10-10 16:19:16 -0300

    componentes novos

--- src/app/components/meu-bar/meu-bar.component.ts
+++ src/app/components/meu-bar/meu-bar.component.ts
@@ -3,10 +3,9 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-meu-bar',
   templateUrl: './meu-bar.component.html',
-  styleUrls: ['./meu-bar.component.css']
+  styleUrls: ['./meu-bar.component.css','./meu-bar.responsive.component .css']
 })
 export class MeuBarComponent implements OnInit {
-
   constructor() { }
 
   ngOnInit(): void {
